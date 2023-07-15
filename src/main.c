303d05f050c83ec9cc3d8689d65f99197b0acc6c
EricWRogers <ericwilliamrogers@gmail.com>
2023-07-14 20:50:44 -0500

    remove vec code

--- src/main.c
+++ src/main.c
@@ -7,51 +7,15 @@
 #include "vec.h"
 
 int main(int argc, char* argv[]) {
+    // array example
+    int wholeNumbers[5];
+
+    wholeNumbers[0] = 0;
+
+    printf("%d\n", wholeNumbers[0]);
+
+    // vec
     double* numbers = NULL;
-
-    vec_init(&numbers, 5, sizeof(double));
-
-    // load vec
-    for(int i = 0; i < 10; i++) {
-        double d = i + 0.5;
-        printf("%lf\n", d);
-        vec_add(&numbers, &d);
-    }
-
-    // print vec
-    unsigned int count = vec_count(&numbers);
-    for(int i = 0; i < count; i++) {
-        printf("%lf\n", numbers[i]);
-    }
-
-    // sort
-    printf("\nSelection Sort\n");
-    vec_selection_sort(&numbers, DoubleDescending);
-
-    // print vec
-    for(int i = 0; i < count; i++) {
-        printf("%lf\n", numbers[i]);
-    }
-
-    // sort
-    printf("\nBubble Sort\n");
-    vec_bubble_sort(&numbers, DoubleAscending);
-
-    // print vec
-    for(int i = 0; i < count; i++) {
-        printf("%lf\n", numbers[i]);
-    }
-
-    // sort
-    printf("\nMerge Sort\n");
-    vec_merge_sort(&numbers, DoubleDescending);
-
-    // print vec
-    for(int i = 0; i < count; i++) {
-        printf("%lf\n", numbers[i]);
-    }
-
-    vec_free(&numbers);
 
     return 0;
 }
